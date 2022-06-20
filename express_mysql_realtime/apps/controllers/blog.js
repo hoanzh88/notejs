279341ed18b610a47b4f3fa1c848430805db7f7f
hoanchuong <hoanchuong@ilavietnam.edu.vn>
2022-06-20 12:01:41 +0700

    chức năng blogs

--- express_mysql_realtime/apps/controllers/blog.js
+++ express_mysql_realtime/apps/controllers/blog.js
@@ -1,8 +1,26 @@
 var express = require("express");
 var router = express.Router();
 
+var post_md = require("../models/post.js");
+
 router.get("/", function(req, res){
-	res.json({"message":"this is blog page"});
+	//res.json({"message":"this is blog page"});
+
+	let data = post_md.getAllPost();
+	data.then(function(posts){
+		console.log("===> ", posts)
+		let data = {
+			posts: posts,
+			error: false
+		}
+		res.render("blog/index.ejs", {data:data});
+	}).catch(function(error){
+		console.log(error);
+		let data = {
+			error: "could get post data"
+		}
+		res.render("blog/index.ejs", {data:data});
+	});
 });
 
 module.exports = router;
